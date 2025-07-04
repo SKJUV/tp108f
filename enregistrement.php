b1d6ef89c3ef8b3b81437288ba1e7067397cf53f
SKJUV <130513705+SKJUV@users.noreply.github.com>
2025-07-04 12:41:59 +0100

    modification enregistrement et adaptation

--- enregistrement.php
+++ enregistrement.php
@@ -4,14 +4,14 @@
 $chemin_destination = "";
 
 
-if (isset($_FILES['FILE']) && $_FILES['FILE']['error'] == 0) {
+if (isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
     
     
     $dossier_destination = 'charger/'; 
     
     // Récupérer les infos du fichier
-    $nom_fichier = basename($_FILES['FILE']['name']);
-    $fichier_temporaire = $_FILES['FILE']['tmp_name'];
+    $nom_fichier = basename($_FILES['file']['name']);
+    $fichier_temporaire = $_FILES['file']['tmp_name'];
 
    
     $chemin_destination = $dossier_destination.$nom_fichier;
@@ -29,14 +29,23 @@ if (isset($_FILES['FILE']) && $_FILES['FILE']['error'] == 0) {
 }
 
 if(isset($_POST)){
-    $con=mysqli_connect("localhost","root","","ict_108");
+    $con=mysqli_connect("localhost","root","","ict108");
     if($con==FALSE){
         echo "erreur d'ouverture";
         exit(0);
     }
-    $request=mysqli_query($con,"INSERT INTO livres (id,titre, sous_titre, auteur, editeur,collections, date_publication, descriptions, genre, langues_disponible,chemin_dacces) VALUES ('".$_POST['ID']."','".$_POST['TITRE']."','".$_POST['SOUS_TITRE']."','".$_POST['NOM_AUTEUR']."','".$_POST['MAISON_EDITION']."','".$_POST['COLLECTION']."','".$_POST['DATE_PUBLICATION']."','".$_POST['DESCRIPTION']."','".$_POST['radio']."','".$_POST['langue']."','".$chemin_destination."')");
+    $request=mysqli_query($con,"INSERT INTO ouvrage (titre_ouvrage,auteur_ouvrage,editeur_ouvrage,ISBN,annee_publication,description_ouvrage,couverture_ouvrage) VALUES ('".$_POST['titre_ouvrage']."','".$_POST['auteur_ouvrage']."','".$_POST['editeur_ouvrage']."','".$_POST['ISBN']."','".$_POST['annee_publication']."','".$_POST['description_ouvrage']."','".$chemin_destination."')");
     if($request==FALSE){
         echo "erreur d'insertion";
+    }else{
+        echo "insertion reussie";
+        echo $_POST['titre_ouvrage'];
+        echo $_POST['auteur_ouvrage'];
+        echo $_POST['editeur_ouvrage'];
+        echo $_POST['ISBN'];
+        echo $_POST['annee_publication'];
+        echo $_POST['description_ouvrage'];
+        echo $chemin_destination;
     }
     $con=mysqli_close($con);
 }
